3a8325b785657538a67c35a0696bee8466ee2c35
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-16 14:11:23 +0530

    [shivam tripathi]: added line comperision

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comaprison Problem");
+            line.check();
+        }
+    }
+}
